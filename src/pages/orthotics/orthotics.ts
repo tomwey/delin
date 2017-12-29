8aedf90baf3fb5fa0f09d67e7854f7a7c903f7f2
tomwey <tomwey@163.com>
2017-12-29 09:43:07 +0800

    ccc

--- src/pages/orthotics/orthotics.ts
+++ src/pages/orthotics/orthotics.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
 
 /**
  * Generated class for the OrthoticsPage page.
@@ -15,11 +15,97 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class OrthoticsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    private actionSheetCtrl: ActionSheetController,
+    public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad OrthoticsPage');
+    // console.log('ionViewDidLoad OrthoticsPage');
   }
+
+  openItem(item)
+  {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: '',
+      buttons: [
+        {
+          text: '发送',
+          handler: () => {
+            // console.log('Archive clicked');
+            // this.viewItem(item);
+          }
+        },
+        {
+          text: '查看',
+          handler: () => {
+            // console.log('Archive clicked');
+            // this.editItem(item);
+          }
+        },
+        {
+          text: '编辑',
+          handler: () => {
+            // console.log('Archive clicked');
+            // this.viewItem(item);
+          }
+        },
+        {
+          text: '改单',
+          handler: () => {
+            // console.log('Archive clicked');
+            // this.editItem(item);
+          }
+        },
+        {
+          text: '退单',
+          handler: () => {
+            // console.log('Archive clicked');
+            // this.editItem(item);
+          }
+        },
+        {
+          text: '删除',
+          role: 'destructive',
+          handler: () => {
+            // console.log('Destructive clicked');
+            // this.deleteItem(item);
+          }
+        },{
+          text: '取消',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  newItem() {
+
+  }
+
+  dataList: any = [
+    {
+      ID: 'jxqsales2017000049',
+      type: '新增',
+      name: 'ML 可调式头颈胸支架',
+      mode: '3.ML.1.1-1001',
+      money: 2180.23,
+      state: 'approving',
+      state_name: '审核中',
+    },
+    {
+      ID: 'jxqsales2017000049',
+      type: '新增',
+      name: 'ML 可调式头颈胸支架',
+      mode: '3.ML.1.1-1001',
+      money: 1920,
+      state: 'approved',
+      state_name: '已审核'
+    },
+  ];
 
 }
