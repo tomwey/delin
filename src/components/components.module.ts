3441354f030dd525fb936c12fe16c27e982832f9
tomwey <tomwey@163.com>
2018-01-31 15:18:53 +0800

    ccc

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AccordionListComponent } from './accordion-list/accordion-list';
 import { IonicPageModule } from 'ionic-angular/module';
-import { CommFormComponent } from './comm-form/comm-form';
+// import { CommFormComponent } from './comm-form/comm-form';
 @NgModule({
-	declarations: [AccordionListComponent,
-    CommFormComponent],
-	imports: [IonicPageModule.forChild(AccordionListComponent)],
-	exports: [AccordionListComponent,
-    CommFormComponent]
+	declarations: [AccordionListComponent],
+	imports: [IonicPageModule.forChild([AccordionListComponent])],
+	exports: [AccordionListComponent]
 })
 export class ComponentsModule {}
--- src/directives/directives.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { AutosizeDirective } from './autosize/autosize';
-
-@NgModule({
-	declarations: [AutosizeDirective],
-	imports: [],
-	exports: [AutosizeDirective]
-})
-export class DirectivesModule {}
