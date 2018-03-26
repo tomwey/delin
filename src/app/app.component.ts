236fc5c17abeaa947a64b7a9ae77bfc9c68f5ca8
tomwey <tomwey@163.com>
2018-03-26 18:07:20 +0800

    ccc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,6 +34,8 @@ export class MyApp {
         // Okay, so the platform is ready and our plugins are available.
         // Here you can do any higher level native things you might need.
         statusBar.styleLightContent();
+
+        statusBar.show();
         
         splashScreen.hide();
       });
